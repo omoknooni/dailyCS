db27b413b5b820849acf5719c179ed51c9aae2ff
omoknooni <hihm0609@gmail.com>
2025-06-05 14:46:31 +0900

    fix: django access db with secret

--- backend/dailycs_backend/settings.py
+++ backend/dailycs_backend/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'dailycs_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME', 'dailycs'),
-        'USER': os.environ.get('DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'qweiop123'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
         'OPTIONS': {
             'charset': 'utf8mb4',
         },
